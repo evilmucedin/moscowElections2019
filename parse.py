8ec32c3f98471c178240f5ca536a84194f8a4b04
Den Raskovalov <denbox@gmail.com>
2019-09-09 19:57:30 -0700

    parse UnitedRussia

--- parse.py
+++ parse.py
@@ -33,10 +33,36 @@ def dequoteV(v):
         res.append(dequote(x))
     return res
 
+unitedRussiaCandidates = set()
+def parseUnitedRussia(filename, markers, antiMarkers):
+    b = open(filename, 'rb').read()
+    s = b.decode('cp1251')
+    for line in s.split("\n"):
+        line = line.strip()
+        if len(re.findall("\<\/td\>", line)) < 4:
+            continue
+        parts = dequoteV(detagV(re.split("\<\/td\>", line)))
+        unitedRussia = False
+        for m in markers:
+            if m in parts[3]:
+                unitedRussia = True
+                break
+        if unitedRussia:
+            for m in antiMarkers:
+                if m in parts[3]:
+                    unitedRussia = False
+        print(parts, unitedRussia)
+        if unitedRussia:
+            unitedRussiaCandidates.add(parts[1])
+
+
 def main():
-    for y in ["2014", "2019"]:
+    parseUnitedRussia("data/candidates2014.html", ["ЕДИНАЯ РОССИЯ"], [])
+    parseUnitedRussia("data/candidates2019.html", ["ЕДИНАЯ РОССИЯ", "Самовыдвижение"], ['Юнеман'])
+    YEARS = ["2014", "2019"]
+    for y in YEARS:
         with open("data/%s.csv" % y, "w") as fOut:
-            print("Station,FullName,Count", file=fOut)
+            print("Station,FullName,Count,UnitedRussia", file=fOut)
             for index in range(1, 46):
                 with open("data/%s/%d.html" % (y, index)) as f:
                     cand = []
@@ -47,8 +73,7 @@ def main():
                         if line.find('Число') >= 0:
                             continue
                         parts = dequoteV(detagV(re.split("\<\/td\>", line)))
-                        print("%d,%s,%s" % (index, parts[1], parts[2]), file=fOut)
-                
+                        print("%d,%s,%s,%d" % (index, parts[1], parts[2], parts[1] in unitedRussiaCandidates), file=fOut)
 
 if __name__ == "__main__":
     main()
