47a6a55d7ee66060f8e6909f2c9653a53ecd7bd4
Den Raskovalov <denbox@gmail.com>
2019-09-09 17:45:10 -0700

    boilerplate

---
+++ download.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Candidates
+# http://www.moscow_city.vybory.izbirkom.ru/region/izbirkom?action=show&root=772000001&tvd=27720001539819&vrn=27720001539308&prver=0&pronetvd=null&region=77&sub_region=0&type=427&vibid=27720001539819
+
+# Early results
+
+# http://www.moscow_city.vybory.izbirkom.ru/region/izbirkom?action=show&root=772000001&tvd=27720002327741&vrn=27720002327736&prver=0&pronetvd=null&region=77&sub_region=0&type=426&vibid=27720002327741
+# http://www.moscow_city.vybory.izbirkom.ru/region/izbirkom?action=show&root=772000001&tvd=27720002327741&vrn=27720002327736&prver=0&pronetvd=null&region=77&sub_region=0&type=426&vibid=27720002327741
+
+# 2014 - tvd=27720001539819
+
+# 2019 - tvd=27720002327740
+
+
+if __name__ == "__main__":
+    download('2014', 'http://www.vybory.izbirkom.ru/region/izbirkom?action=show&vrn=27720001539308&region=77&prver=0&pronetvd=null')
