7e5258ac631df9ab415c61ae790b4ee178b84ed2
Den Raskovalov <denbox@gmail.com>
2019-09-19 20:29:00 -0700

    extra stat

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ import math
 
 def main():
     stats = []
+    bestUrs = {}
     for y in ["2014", "2019"]:
         print(y)
         df = pandas.read_csv('data/%s.csv' % y)
@@ -16,6 +17,8 @@ def main():
             count1 = 0
             countSum = 0
             index = 0
+            bestUr = 0
+            st = dfStation[0]
             for _, row in dfStation[1].iterrows():
                 c = float(row['Count'])
                 if index == 0:
@@ -25,6 +28,9 @@ def main():
                 countSum += c
                 index += 1
                 s += c
+                if not bestUr and row['UnitedRussia']:
+                    bestUr = c
+            bestUrs.setdefault(st, []).append(bestUr)
             # print(countSum, count0, count1)
             rem = float(countSum - count0 - count1) / countSum / (index - 2)
             print(rem)
@@ -33,6 +39,10 @@ def main():
         print(st)
         stats.append(st)
     print('T=%f' % ((stats[0][1] - stats[1][1])/math.sqrt(stats[0][2] + stats[1][2])))
+
+    for k, v in bestUrs.items():
+        if v[0]:
+            print(k, v, float(v[1])/v[0])
 
 if __name__ == "__main__":
     main()
